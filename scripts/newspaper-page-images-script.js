00c14e5af2f1223695dec56489d0c5249220e1fd
Tim Sherratt <tim@discontents.com.au>
2024-04-16 23:52:11 +1000

    Deployed 659c39b with MkDocs version: 1.5.3

--- scripts/newspaper-page-images-script.js
+++ scripts/newspaper-page-images-script.js
@@ -18,25 +18,35 @@ btn.addEventListener('click', async function() {
     let pageID;
     if (identifier.startsWith("http")) {
         if (identifier.indexOf("article") >= 0) {
-            articleID = identifier.match(/article\/?(\d+)/)[1];
-            pageID = await getPageID(articleID);
+            document.getElementById("status").style.display = 'none';
+            output.innerHTML = "You need to supply a page url not an article url";
+            // articleID = identifier.match(/article\/?(\d+)/)[1];
+            // pageID = await getPageID(articleID);
         } else {
             pageID = identifier.match(/page\/?(\d+)/)[1];
         }
     } else {
         pageID = identifier;
     }
-    url = "https://trove.nla.gov.au/ndp/imageservice/nla.news-page" + pageID + "/level4";
-    document.getElementById("status").style.display = 'none';
-    console.log(url);
-    let link = document.createElement("a");
-    link.textContent = "nla.news-page" + pageID;
-    link.href = url;
-    link.target = "_blank";
-    let image_list = document.createElement("ul");
-    let list_item = document.createElement("li");
-    list_item.appendChild(link);
-    image_list.appendChild(list_item);
-    output.appendChild(image_list);
+    if (pageID) {
+        url = "https://trove.nla.gov.au/ndp/imageservice/nla.news-page" + pageID + "/level4";
+        document.getElementById("status").style.display = 'none';
+        console.log(url);
+        let link = document.createElement("a");
+        link.textContent = "nla.news-page" + pageID;
+        link.href = url;
+        link.target = "_blank";
+        let image_list = document.createElement("ul");
+        let list_item = document.createElement("li");
+        list_item.appendChild(link);
+        image_list.appendChild(list_item);
+        output.appendChild(image_list);
+    }
+});
+
+let clear = document.getElementById('clear-image-results');
+clear.addEventListener('click', async function() {
+    document.getElementById("images").innerHTML = "";
+    document.getElementById("page-id").value = "";
 });
 document.getElementById("status").style.display = 'none';
